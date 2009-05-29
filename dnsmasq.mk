86f35aeb331c4008db0b1b2a451b55a1f749b824
Jose Luis Duran <jlduran@gmail.com>
2009-05-29 04:38:07 -0500

    Fixed Dnsmasq

--- dnsmasq.mk
+++ dnsmasq.mk
@@ -73,9 +73,9 @@ define Package/$(PKG_NAME)
   CATEGORY:=Network
   TITLE:=Dnsmasq
   DESCRIPTION:=\
-        Dnsmasq is a lightweight, easy to configure DNS forwarder and \\\
-        DHCP server. It is designed to provide DNS, and optionally DHCP, \\\
-        to a small network. It can serve the names of local machines \\\
+	Dnsmasq is a lightweight, easy to configure DNS forwarder and \\\
+	DHCP server. It is designed to provide DNS, and optionally DHCP, \\\
+	to a small network. It can serve the names of local machines \\\
 	which are not in the global DNS. The DHCP server integrates with \\\
 	the DNS server and allows machines with DHCP-allocated addresses \\\
 	to appear in the DNS with names configured either in each host \\\
