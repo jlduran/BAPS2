e25adbca5e1e339147403ece5e2daca4219abaf3
Jose Luis Duran <jlduran@gmail.com>
2009-05-29 04:34:06 -0500

    Fixed Dnsmasq

--- dnsmasq.mk
+++ dnsmasq.mk
@@ -31,19 +31,7 @@ PKG_BUILD_DIR:=$(BUILD_DIR)/tmp/dnsmasq
 #                   Disable IPv6 & Read-only TFTP Server
 #--------------------------------------------------------------------------
 
-DNSMASQ_COPTS=
-
-ifneq ($(BR2_INET_IPV6),y)
-DNSMASQ_COPTS+=-DNO_IPV6
-endif
-
-ifneq ($(BR2_PACKAGE_DNSMASQ_TFTP),y)
-DNSMASQ_COPTS+=-DNO_TFTP
-endif
-
-ifneq ($(BR2_LARGEFILE),y)
-DNSMASQ_COPTS+=-DNO_LARGEFILE
-endif
+DNSMASQ_COPTS=-DNO_IPV6 -DNO_TFTP -DNO_LARGEFILE
 
 #--------------------------------------------------------------------------
 
@@ -85,14 +73,14 @@ define Package/$(PKG_NAME)
   CATEGORY:=Network
   TITLE:=Dnsmasq
   DESCRIPTION:=\
-        Dnsmasq is a lightweight, easy to configure DNS forwarder and\\\
-        DHCP server. It is designed to provide DNS, and optionally DHCP,\\\
-        to a small network. It can serve the names of local machines\\\
-	which are not in the global DNS. The DHCP server integrates with\\\
-	the DNS server and allows machines with DHCP-allocated addresses\\\
-	to appear in the DNS with names configured either in each host\\\
-	or in a central configuration file. Dnsmasq supports static and\\\
-	dynamic DHCP leases and BOOTP/TFTP for network booting of\\\
+        Dnsmasq is a lightweight, easy to configure DNS forwarder and \\\
+        DHCP server. It is designed to provide DNS, and optionally DHCP, \\\
+        to a small network. It can serve the names of local machines \\\
+	which are not in the global DNS. The DHCP server integrates with \\\
+	the DNS server and allows machines with DHCP-allocated addresses \\\
+	to appear in the DNS with names configured either in each host \\\
+	or in a central configuration file. Dnsmasq supports static and \\\
+	dynamic DHCP leases and BOOTP/TFTP for network booting of \\\
 	diskless machines.
   URL:=http://thekelleys.org.uk/dnsmasq/doc.html
 endef
