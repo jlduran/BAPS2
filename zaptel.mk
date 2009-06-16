04845ecf10f0823d351e32520a685b640abbe516
Jose Luis Duran <jlduran@gmail.com>
2009-06-16 04:36:58 -0500

    Unified zaptel package.

--- zaptel.mk
+++ zaptel.mk
@@ -3,6 +3,29 @@
 # Jose Luis Duran April 2009
 #
 # make -f zaptel.mk zaptel-package
+#
+# ZAPTEL_GSM_MODULE=y/n enables/disables support for uCpbx's GSM1 module.
+# ZAPTEL_SPI_INTERFACE with ZAPTEL_GSM_MODULE currently not supported.
+# http://www.ucpbx.com/files/file/GSM1_User_Manual.pdf
+#
+# Choose just one (1) of the lines below:
+#   ZAPTEL_SPI_INTERFACE uses the Blackfin SPI hardware to talk
+#   to the analog modules. This method is used for the earlier IP04s
+#   and all IP04s shipped by Rowetel. Unfortunately it means the MMC
+#   card can't be used, as zaptel hogs the SPI bus!
+#
+#   ZAPTEL_SPORT_INTERFACE uses the SPORT1 hardware to implement
+#   a SPI port to talk to the analog modules.  This fees up the SPI
+#   bus for the MMC card. This was developed by Alex Tao (thanks Alex)
+#   and is likely to be the standard for the future.
+#
+# Note that different CPLD firmware is required to support each SPI
+# method, if your IP04 won't detect modules this is a sign you may
+# have the wrong CPLD firmware and/or driver.
+
+#ZAPTEL_SPI_INTERFACE=y
+ZAPTEL_SPORT_INTERFACE=y
+ZAPTEL_GSM_MODULE=n
 
 include rules.mk
 
@@ -13,7 +36,12 @@ ZAPTEL_SOURCE=$(ZAPTEL_NAME).tar.gz
 ZAPTEL_SITE=http://downloads.digium.com/pub/zaptel/releases
 ZAPTEL_UNZIP=zcat
 ZAPTEL_TOPDIR_MODULES:="zaptel bfsi wcfxs sport_interface ztdummy"
-ZAPTEL_EXTRA_CFLAGS=-DCONFIG_4FX_SPORT_INTERFACE -DCONFIG_CALC_XLAW
+ZAPTEL_EXTRA_CFLAGS+=-DSTANDALONE_ZAPATA -DCONFIG_CALC_XLAW
+ifeq ($(strip $(ZAPTEL_SPORT_INTERFACE)),y)
+ZAPTEL_EXTRA_CFLAGS+=-DCONFIG_4FX_SPORT_INTERFACE
+else
+ZAPTEL_EXTRA_CFLAGS+=-DCONFIG_4FX_SPI_INTERFACE
+endif
 OSLEC_DIR=$(BUILD_DIR)/oslec
 
 STAGING_INC=$(STAGING_DIR)/usr/include
@@ -24,9 +52,17 @@ MOD_DIR:=$(shell ls $(UCLINUX_DIST)/root/lib/modules)
 
 PKG_VERSION:=1.4.9.2
 PKG_RELEASE:=1
-
 PKG_NAME:=zaptel
-COMMENT:=SPI-over-SPORT1 version with GSM module support. 
+
+ifeq ($(strip $(ZAPTEL_SPORT_INTERFACE)),y)
+ifeq ($(strip $(ZAPTEL_GSM_MODULE)),y)
+COMMENT:=with support for uCpbx GSM1 module SPI-over-SPORT1 version (later Atcom IP04s, IP08s).
+else
+COMMENT:=SPI-over-SPORT1 version (later Atcom IP04s, IP08s).
+endif
+else
+COMMENT:=SPI-over-SPI hardware version (Rowetel IP04).
+endif
 
 TARGET_DIR=$(TOPDIR)/tmp/$(PKG_NAME)/ipkg/$(PKG_NAME)
 
@@ -46,14 +82,20 @@ $(ZAPTEL_DIR)/.configured: $(ZAPTEL_DIR)/.unpacked
 
 	# we use sym-links so that any changes we make to zaptel
 	# get captured by SVN
-
+ifeq ($(strip $(ZAPTEL_GSM_MODULE)),y)
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/sport_interface.c $(ZAPTEL_DIR)/kernel/sport_interface.c
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/wcfxs.c $(ZAPTEL_DIR)/kernel/wcfxs.c
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/fx.c $(ZAPTEL_DIR)/kernel/fx.c
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/bfsi.c $(ZAPTEL_DIR)/kernel/bfsi.c
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/gsm_module.c $(ZAPTEL_DIR)/kernel/gsm_module.c
 	ln -sf $(BUILD_DIR)/src/zaptel-gsm/GSM_module_SPI.h $(ZAPTEL_DIR)/kernel/GSM_module_SPI.h
-	cp -f $(BUILD_DIR)/src/zaptel-gsm/zaptel-base.c-1.4.9.2 $(ZAPTEL_DIR)/kernel/zaptel-base.c
+	ln -sf $(BUILD_DIR)/src/zaptel-gsm/zaptel-base.c-1.4.9.2 $(ZAPTEL_DIR)/kernel/zaptel-base.c
+else
+	ln -sf $(BUILD_DIR)/src/sport_interface.c $(ZAPTEL_DIR)/kernel/sport_interface.c
+	ln -sf $(BUILD_DIR)/src/wcfxs.c $(ZAPTEL_DIR)/kernel/wcfxs.c
+	ln -sf $(BUILD_DIR)/src/fx.c $(ZAPTEL_DIR)/kernel/fx.c
+	ln -sf $(BUILD_DIR)/src/bfsi.c $(ZAPTEL_DIR)/kernel/bfsi.c
+endif
 	ln -sf $(BUILD_DIR)/src/sport_interface.h $(ZAPTEL_DIR)/kernel/sport_interface.h
 	ln -sf $(BUILD_DIR)/src/wcfxs.h $(ZAPTEL_DIR)/kernel/wcfxs.h
 	ln -sf $(BUILD_DIR)/src/bfsi.h $(ZAPTEL_DIR)/kernel/bfsi.h
@@ -113,24 +155,27 @@ zaptel: $(ZAPTEL_DIR)/.configured
 	mkdir -p $(TARGET_DIR)/lib/modules/$(MOD_DIR)/misc
 	mkdir -p $(TARGET_DIR)/bin
 	mkdir -p $(TARGET_DIR)/etc/init.d 
-	mkdir -p $(TARGET_DIR)/etc/asterisk
+	#mkdir -p $(TARGET_DIR)/etc/asterisk
 
 	# install
 
 	cp -f $(ZAPTEL_DIR)/kernel/zaptel.ko $(ZAPTEL_DIR)/kernel/wcfxs.ko \
 	$(ZAPTEL_DIR)/kernel/sport_interface.ko $(ZAPTEL_DIR)/kernel/bfsi.ko \
 	$(TARGET_DIR)/lib/modules/$(MOD_DIR)/misc
-
 	cp -f $(ZAPTEL_DIR)/kernel/ztdummy.ko \
 	$(TARGET_DIR)/lib/modules/$(MOD_DIR)/misc
 	cp -f $(ZAPTEL_DIR)/ztcfg $(ZAPTEL_DIR)/zapscan $(ZAPTEL_DIR)/ztscan $ $(ZAPTEL_DIR)/fxstest $(TARGET_DIR)/bin
 	cp files/zaptel.init $(TARGET_DIR)/etc/init.d/zaptel
 	chmod a+x $(TARGET_DIR)/etc/init.d/zaptel
 	cp -f files/zaptel.conf.in $(TARGET_DIR)/etc/zaptel.conf
+	#cp -f files/zapata.conf.in $(TARGET_DIR)/etc/asterisk
 
 	touch $(PKG_BUILD_DIR)/.built
 
 all: zaptel
+
+zaptel-clean:
+	make -C $(ZAPTEL_DIR) clean
 
 zaptel-dirclean:
 	rm -Rf $(ZAPTEL_DIR)
@@ -158,9 +203,9 @@ define Package/$(PKG_NAME)
   CATEGORY:=Network
   TITLE:=Zaptel
   DESCRIPTION:=\
-        Telephony hardware drivers for IP04 with support for Astfin GSM module $(COMMENT) 
+        Telephony hardware drivers for IP04 $(COMMENT) 
   DEPENDS:=oslec
-  URL:=http://www.ucpbx.com/files/file/GSM1_User_Manual.pdf
+  URL:=http://www.asterisk.org
 endef
 
 # post installation - add the modules.dep entries
